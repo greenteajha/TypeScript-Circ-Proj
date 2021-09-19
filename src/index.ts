8462224f9e85f67b2a4a91239ed7ffcd21e12b73
Jeremy Teo Zheng Cong <jeremyteo88@hotmail.com>
2021-09-19 22:09:06 +0800

    Added layout.ts and init.ts. Imported to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("HELLO WORLD!");
+import { init } from "./init";
+import { renderLayout } from "./layout";
+
+const root = init();
+
+const { layout } = renderLayout();
+
+root.appendChild(layout);
---
+++ src/layout.ts
@@ -0,0 +1,64 @@
+let colorMode = "light";
+
+export function renderLayout(){
+    const layout = document.createElement("div");
+    layout.id = "layout";
+    layout.classList.add("container");
+
+    const nav = document.createElement("nav");
+    nav.classList.add("row");
+    nav.classList.add("space-between");
+    layout.appendChild(nav);
+
+    const navList = document.createElement("ul");
+    navList.classList.add("row");
+    navList.classList.add("nav");
+    nav.appendChild(navList);
+
+    const topItem = document.createElement("li");
+    navList.appendChild(topItem);
+    const topLink = document.createElement("a");
+    topLink.innerText = "Top";
+    topLink.classList.add("active");
+    topLink.classList.add("nav-link");
+    topLink.href = "#";
+    topLink.addEventListener("click", () =>
+        document.dispatchEvent(new Event("topLink"))
+    );
+    topItem.appendChild(topLink);
+
+    const newItem = document.createElement("li");
+    navList.appendChild(newItem);
+    const newLink = document.createElement("a");
+    newLink.innerText = "New";
+    newLink.classList.add("nav-link");
+    newLink.href = "#";
+    newLink.addEventListener("click", () =>
+        document.dispatchEvent(new Event("newLink"))
+    );
+    newItem.appendChild(newLink);
+
+    const darkModeToggle = document.createElement("button");
+    darkModeToggle.innerText = "🔦";
+    darkModeToggle.classList.add("btn-clear");
+    darkModeToggle.style.fontSize = "30px";
+
+    darkModeToggle.addEventListener("click", () => {
+        if (colorMode === "light"){
+            colorMode = "dark";
+            document.body.classList.add("dark");
+            darkModeToggle.innerText = "💡";
+        }else{
+            colorMode = "light";
+            document.body.classList.remove("dark");
+            darkModeToggle.innerText = "🔦";
+        }
+    });
+
+    nav.appendChild(darkModeToggle);
+
+
+
+
+    return {layout};
+}
