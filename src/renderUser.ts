61e3f4a734e17534a9de5760cd802f8813391ad5
Jeremy Teo Zheng Cong <jeremyteo88@hotmail.com>
2021-09-20 21:30:28 +0800

    Started renderUser.ts

---
+++ src/renderUser.ts
@@ -0,0 +1,7 @@
+import { getItem, getUser } from "./api"
+import { createStory } from "./createStory";
+import { renderLoader } from "./renderLoader";
+import { formatDate } from "./util/formatDate";
+
+
+
