38375cdd83b247a6255cfeba1b659b2f2d574307
Jeremy Teo <jeremyteo88@hotmail.com>
2021-09-20 08:00:05 +0800

    Completed api.tx

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,7 @@
 const USER_URL_BASE = "https://hacker-news.firebaseio.com/v0/user/";
 const STORY_URL_BASE = "https://hacker-news.firebaseio.com/v0/item/";
+const TOP_POSTS_URL = "https://hacker-news.firebaseio.com/v0/topstories.json";
+const NEW_POSTS_URL = "https://hacker-news.firebaseio.com/v0/newstories.json";
 
 function getStoryUrl(story: number){
     return `${STORY_URL_BASE}${story}.json`;
@@ -32,4 +34,31 @@ export interface User {
 
 let itemCache: { [itemId: number]: Story } = {};
 
-console.log(itemCache[1]);
+export async function getItem(id: number): Promise<Story>{
+    if (itemCache[id]) return itemCache[id];
+    const item = await (await fetch(getStoryUrl(id))).json();
+    itemCache[id] = item;
+    return item;
+}
+
+export async function getStories(which: "top" | "new"){
+    const url = which === "top" ? TOP_POSTS_URL : NEW_POSTS_URL;
+    const postIDs: number[] = await (await fetch(url)).json();
+    return Promise.all(postIDs.slice(0,30).map(getItem));
+}
+
+export async function getComments(storyId: number){
+    const story = await getItem(storyId);
+
+    const comments = story.kids.slice(0,30).map((id) => getItem);
+    return Promise.all(comments);
+}
+
+let userCache: { [username: string]: User } = {};
+
+export async function getUser(username: string){
+    if(userCache[username]) return userCache;
+    const user: User = await (await fetch(getUserUrl(username))).json();
+    userCache[username] = user;
+    return user;
+}
